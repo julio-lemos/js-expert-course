c2182014b30c9e48217e4f1003305d1e052e74b4
Julio Lemos <julio.santana@justa.com.vc>
2022-06-18 12:17:38 -0300

    module2: trabalhando com spies

---
+++ projects/modulo2-js-testing/aula03-spies/src/fibonacci.js
@@ -0,0 +1,14 @@
+class Fibonacci {
+  /** Generator responsável por executar o padrão fibonacci */
+  *execute(input, current = 0, next = 1) {
+    if (input == 0) return 0;
+
+    /** Retorna o valor */
+    yield current;
+
+    /** Delega a função, mas não retorna valor */
+    yield* this.execute(input - 1, next, current + next);
+  }
+}
+
+module.exports = Fibonacci;
